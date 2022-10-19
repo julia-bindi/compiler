216eb20b806a2f5f92349b807ccddae4ac52d209
arthur-sabioni <arthur.lsabioni@gmail.com>
2022-10-19 09:33:57 -0300

    Add comments to grammar

--- src/Syntatical/Syntatic.java
+++ src/Syntatical/Syntatic.java
@@ -54,6 +54,7 @@ public class Syntatic {
     };
 
     private void program(){
+        // program -> start [decl-list] stmt-list exit
         this.eat(Tag.START);
         switch(this.t.tag){
             case Tag.INT:
@@ -68,6 +69,7 @@ public class Syntatic {
     }
 
     private void stmtList(){
+        // stmt-list -> stmt {stmt}
         do{
             this.stmt();
         } while(this.t.tag == Tag.ID ||
@@ -79,20 +81,25 @@ public class Syntatic {
 
     private void stmt(){
         switch(this.t.tag){
+            // stmt -> assign-stmt ;
             case Tag.ID:
                 this.assignStmt();
                 this.eat(Tag.SEMICOLON);
                 break;
+            // stmt -> if-stmt
             case Tag.IF:
                 this.ifStmt();
                 break;
+            // stmt -> while-stmt
             case Tag.DO:
                 this.whileStmt();
                 break;
+            // stmt -> read-stmt ;
             case Tag.SCAN:
                 this.readStmt();
                 this.eat(Tag.SEMICOLON);
                 break;
+            // stmt -> write-stmt ;
             case Tag.PRINT:
                 this.writeStmt();
                 this.eat(Tag.SEMICOLON);
@@ -103,12 +110,14 @@ public class Syntatic {
     }
 
     private void assignStmt(){
+        // assign-stmt -> identifier = simple-expr
         this.eat(Tag.ID);
         this.eat(Tag.ASSIGN);
         this.simpleExpr();
     }
 
     private void ifStmt(){
+        // if-stmt -> if condition then stmt-list [else stmt-list] end
         this.eat(Tag.IF);
         this.condition();
         this.eat(Tag.THEN);
@@ -121,22 +130,26 @@ public class Syntatic {
     }
 
     private void condition(){
+        // condition -> expression
         this.expression();
     }
 
     private void whileStmt(){
+        // while-stmt -> do stmt-list stmt-sufix
         this.eat(Tag.DO);
         this.stmtList();
         this.stmtSufix();
     }
 
     private void stmtSufix(){
+        // stmt-sufix -> while condition end
         this.eat(Tag.WHILE);
         this.condition();
         this.eat(Tag.END);
     }
 
     private void readStmt(){
+        // read-stmt -> scan ( identifier )
         this.eat(Tag.SCAN);
         this.eat(Tag.OPEN_PAR);
         this.eat(Tag.ID);
@@ -144,6 +157,7 @@ public class Syntatic {
     }
 
     private void writeStmt(){
+        // write-stmt -> print ( writable )
         this.eat(Tag.PRINT);
         this.eat(Tag.OPEN_PAR);
         this.writable();
@@ -151,6 +165,7 @@ public class Syntatic {
     }
 
     private void writable(){
+        // writable -> simple-expr
         switch(this.t.tag){
             case Tag.NOT:
             case Tag.SUB:
@@ -166,11 +181,13 @@ public class Syntatic {
     }
 
     private void expression(){
+        // expressin -> simple-expr expression-aux
         this.simpleExpr();
         this.expressionAux();
     }
 
     private void expressionAux(){
+        // expression-aux -> [relop expression]
         switch(this.t.tag){
             case Tag.EQ:
                 this.eat(Tag.EQ);
@@ -196,17 +213,20 @@ public class Syntatic {
                 this.eat(Tag.NE);
                 this.expression();
                 break;
-            default: //lambda
+            // expression-aux -> lambda
+            default:
                 break;
         }
     }
 
     private void simpleExpr(){
+        // simple-expr -> term simple-expr-aux
         this.term();
         this.simpleExprAux();
     }
 
     private void simpleExprAux(){
+        // simple-expr-aux -> [addop simple-expr]
         switch(this.t.tag){
             case Tag.ADD:
                 this.eat(Tag.ADD);
@@ -220,17 +240,20 @@ public class Syntatic {
                 this.eat(Tag.OR);
                 this.simpleExpr();
                 break;
-            default: //lambda
+            // simple-expr-aux -> lambda
+            default:
                 break;
         }
     }
 
     private void term(){
+        // term -> dactor-a term-aux
         this.factorA();
         this.termAux();
     }
 
     private void termAux(){
+        // term-aux -> [mulop term]
         switch(this.t.tag){
             case Tag.MULT:
                 this.eat(Tag.MULT);
@@ -244,21 +267,25 @@ public class Syntatic {
                 this.eat(Tag.AND);
                 this.term();
                 break;
-            default: //lambda
+            // term-aux -> lambda
+            default:
                 break;
         }
     }
 
     private void factorA(){
         switch(this.t.tag){
+            // factor-a -> ! factor
             case Tag.NOT:
                 this.eat(Tag.NOT);
                 this.factor();
                 break;
+            // factor-a -> - factor
             case Tag.SUB:
                 this.eat(Tag.SUB);
                 this.factor();
                 break;
+            // factor-a -> factor
             case Tag.ID:
             case Tag.NUMERIC:
             case Tag.LITERAL:
@@ -272,15 +299,18 @@ public class Syntatic {
 
     private void factor(){
         switch(this.t.tag){
+            // factor -> identifier
             case Tag.ID:
                 this.eat(Tag.ID);
                 break;
+            // factor -> constant -> integer_const | numeric_const | literal
             case Tag.NUMERIC:
                 this.eat(Tag.NUMERIC);
                 break;
             case Tag.LITERAL:
                 this.eat(Tag.LITERAL);
                 break;
+            // factor -> ( expression )
             case Tag.OPEN_PAR:
                 this.eat(Tag.OPEN_PAR);
                 this.expression();
@@ -292,6 +322,7 @@ public class Syntatic {
     }
 
     private void declList(){
+        // decl-list -> decl {decl}
         do{
             this.decl();
         } while(this.t.tag == Tag.FLOAT ||
@@ -300,6 +331,7 @@ public class Syntatic {
     }
 
     private void decl(){
+        // decl -> type ident-list ;
         this.type();
         this.identList();
         this.eat(Tag.SEMICOLON);
@@ -307,12 +339,15 @@ public class Syntatic {
 
     private void type(){
         switch(this.t.tag){
+            // type -> int
             case Tag.INT:
                 this.eat(Tag.INT);
                 break;
+            // type -> float
             case Tag.FLOAT:
                 this.eat(Tag.FLOAT);
                 break;
+            // type -> string
             case Tag.STRING:
                 this.eat(Tag.STRING);
                 break;
@@ -323,14 +358,11 @@ public class Syntatic {
     }
 
     private void identList(){
+        // ident-list -> identifier {, identifier}
         this.eat(Tag.ID);
-        switch(this.t.tag){
-            case Tag.COLON:
-                while(this.t.tag == Tag.COLON){
-                    this.eat(Tag.COLON);
-                    this.eat(Tag.ID);
-                }
-                break;
+        while(this.t.tag == Tag.COLON){
+            this.eat(Tag.COLON);
+            this.eat(Tag.ID);
         }
     }
 
